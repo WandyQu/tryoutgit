8ab792545a977b05f6ea1991e77dcd4d17d93d37
wandy <wandy.qu@shareworks.cn>
2016-12-06 23:34:57 -0600

    Add test2

--- src/test2.java
+++ src/test2.java
@@ -3,5 +3,9 @@ package com.mgit.trygit;
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+
     }
 }
