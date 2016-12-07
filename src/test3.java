75d1939060d187941e58b90a53aef8dd71659a1d
wandy <wandy.qu@shareworks.cn>
2016-12-07 00:10:36 -0600

    Add test3

---
+++ src/test3.java
@@ -0,0 +1,11 @@
+package com.mgit.trygit;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+
+    }
+}
